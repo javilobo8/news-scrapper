7c207347dfda95d42440be8405561613d6023395
javilobo8 <jbermudezlobo@gmail.com>
2018-12-16 12:08:49 +0100

    update readme

--- README.md
+++ README.md
@@ -10,3 +10,65 @@ Application for save/send posts from scrapping webs. Useful for news, forums, li
 * Error handling. - DONE
 * Implement `maxRetries` client option.
 * Tests.
+
+## Targets
+
+Each webpage
+
+```javascript
+const targetWithAxios = {
+  name: 'Reddit - SwitchHaxing/New', // Name
+  scrapperName: 'reddit-api', // Scrapper name (in this case src/scrappers/reddit-api.scrapper.js)
+  description: 'Reddit SwitchHaxing New posts', // Description
+  tags: ['switch'], // Some optional tags
+  http: {
+    client: 'axios', // Could be axios or puppeteer
+    clientOptions: {
+      maxRetries: undefined, // Defaults to 0 (only axios)
+      timeout: undefined, // TBI
+      waitPageLoad: undefined, // Defaults to 0 (only puppeteer)
+    },
+    host: 'https://www.reddit.com', // Host
+    path: '/r/SwitchHaxing/new.json', // Path
+    method: 'get', // Method
+    params: { // Query Params
+      search: 'optional',
+    }
+  },
+};
+
+const targetWithPuppeteer =   {
+  name: 'PreDB',
+  scrapperName: 'predb',
+  description: 'Switch releases list page',
+  tags: ['switch'],
+  http: {
+    client: 'puppeteer',
+    clientOptions: {
+      maxRetries: undefined,
+      timeout: undefined,
+      waitPageLoad: 5000,
+    },
+    host: 'https://predb.me',
+    path: '',
+    method: 'get',
+    params: {
+      search: 'nsw',
+    },
+  },
+};
+```
+
+## Tasks
+
+```javascript
+const taskExample = {
+  name: 'TASK #1',
+  description: 'Test task number 1',
+  active: true, // Enable or disable task
+  startOnCreate: true, // Start when application starts (depends on active)
+  cron: '* * * * *', // Cron-based syntax
+  targets: [], // TargetIds
+  emails: ['test@test.com'], // Email to send something TBI
+}
+```
--- src/services/request.service.js
+++ src/services/request.service.js
@@ -27,34 +27,53 @@ class RequestService {
     const params = http.params ? `?${qs.stringify(http.params)}` : '';
     const requestUrl = url.resolve(http.host, http.path);
 
-    this.log(`Requesting with axios ${http.method} ${url.resolve(requestUrl, params)}`);
-    const before = Date.now();
-    const response = await axios({
-      url: requestUrl,
-      method: http.method,
-      params: http.params,
-      body: http.body,
-      timeout: http.clientOptions.timeout,
-    });
-    this.log(`Responsed ${http.method} ${http.host}${http.path} OK! (${Date.now() - before}ms)`);
+    let requests = 0;
 
-    return response.data;
+    const request = async () => {
+      this.log(`Requesting with axios ${http.method} ${url.resolve(requestUrl, params)}`);
+      const before = Date.now();
+
+      let response;
+
+      try {
+        response = await axios({
+          url: requestUrl,
+          method: http.method,
+          params: http.params,
+          body: http.body,
+          timeout: http.clientOptions.timeout,
+        });
+      } catch (err) {
+        this.log.warn(`WARNING ${http.method} ${http.host}${http.path} KO! (${Date.now() - before}ms)`);
+        requests++;
+        if (requests < (http.clientOptions.maxRetries || 0)) {
+          this.log.warn(`Retrying ${http.method} ${http.host}${http.path}`);
+          return request();
+        }
+        this.log.error(`ERROR ${http.method} ${http.host}${http.path} KO! (${Date.now() - before}ms)`);
+        throw err;
+      }
+
+      this.log(`Responsed ${http.method} ${http.host}${http.path} OK! (${Date.now() - before}ms)`);
+      return response.data;
+    };
+
+    return request();
   }
 
   async _getWithPuppeter(http) {
     const params = http.params ? `?${qs.stringify(http.params)}` : '';
     const requestUrl = url.resolve(url.resolve(http.host, http.path), params);
 
-    this.log(`Creating puppeteer browser for ${http.method} ${requestUrl}`);
+    this.log(`Requesting with puppeteer ${http.method} ${requestUrl}`);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    this.log(`Requesting with puppeteer ${http.method} ${requestUrl}`);
     const before = Date.now();
     await page.goto(requestUrl);
     this.log(`Responsed ${http.method} ${requestUrl} OK! (${Date.now() - before}ms)`);
 
-    const waitPageLoad = http.clientOptions.waitPageLoad || 0;
+    const waitPageLoad = (http.clientOptions.waitPageLoad || 0);
     if (waitPageLoad) {
       this.log(`Waiting for ${waitPageLoad}ms ${http.method} ${requestUrl}`);
       await page.waitFor(waitPageLoad);
